2d68cc41aa5955c88a68eb1199c0f65ab5c60db8
Nambi R <120433737+nambir01@users.noreply.github.com>
2025-07-27 21:28:45 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "k8s-bucket2906"
+    bucket         = "nambirbucket2707"
     region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-files"
+    dynamodb_table = "nambi-dbtable"
     encrypt        = true
   }
   required_version = ">=0.13.0"
